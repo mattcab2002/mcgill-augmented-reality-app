a69a04bb3fc332ce724c5ed62810fb8869415bc7
adityanegii <aditya.negi@mail.mcgill.ca>
2023-02-13 16:43:56 -0500

    cleaned up

--- mcgill-augmented-reality-backend/src/test/java/mcgillar/backend/user/LoginTest.java
+++ mcgill-augmented-reality-backend/src/test/java/mcgillar/backend/user/LoginTest.java
@@ -11,9 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import mcgillar.backend.services.AppUserService;
 
@@ -26,38 +26,27 @@ public class LoginTest {
     @Autowired
     private AppUserService appUserService;
 
+    @BeforeTestClass
     @Test
     public void loginSuccess() {
         String username = "test_name";
         String pass = "test_password";
         appUserService.createUser(username, pass);
-        HttpHeaders headers = new HttpHeaders();
-        String creds = username + ":" + pass;
-        byte[] plainCreds = creds.getBytes();
-        byte[] base64PlainCreds = Base64.getEncoder().encode(plainCreds);
-        String newCreds = new String(base64PlainCreds);
-        
-        headers.add("Authorization", "Basic " + newCreds);
-        HttpEntity<Object> entity = new HttpEntity<Object>(null, headers);
-        ResponseEntity<String> token = client.postForEntity("/token", entity, String.class);
 
-        String body = token.getBody();
-
-        HttpHeaders headers2 = new HttpHeaders();
-        headers2.add("Authorization", "Bearer " + body);
-
-        HttpEntity<Object> entity2 = new HttpEntity<Object>(null, headers2);
-        // ResponseEntity<String> response = client.getForEntity("/", entity2, String.class);
-        ResponseEntity<String> response = client.exchange("/", HttpMethod.GET, entity2, String.class);
-        
+        ResponseEntity<String> response = getResponse(username, pass);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-          
     }
 
     @Test
     public void loginBad() {
         String username = "DNE";
         String pass = "DNE";
+
+        ResponseEntity<String> response = getResponse(username, pass);
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+
+    private ResponseEntity<String> getResponse(String username, String pass) {
         HttpHeaders headers = new HttpHeaders();
         String creds = username + ":" + pass;
         byte[] plainCreds = creds.getBytes();
@@ -66,9 +55,7 @@ public class LoginTest {
         
         headers.add("Authorization", "Basic " + newCreds);
         HttpEntity<Object> entity = new HttpEntity<Object>(null, headers);
-        ResponseEntity<String> token = client.postForEntity("/token", entity, String.class);
-
-        
-        assertEquals(HttpStatus.UNAUTHORIZED, token.getStatusCode());
+        ResponseEntity<String> response = client.postForEntity("/token", entity, String.class);
+        return response;
     }
 }
