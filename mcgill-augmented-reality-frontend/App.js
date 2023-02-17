a01f34374c7de88dbd3d2da304110e11b0b999c3
Yoah Qiu <yoah.qiu@gmail.com>
2023-02-17 01:17:48 -0500

    Refactor Home screen

--- mcgill-augmented-reality-frontend/App.js
+++ mcgill-augmented-reality-frontend/App.js
@@ -1,56 +1,10 @@
 import React from 'react';
-import {Pressable, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
-const image = {uri: 'https://www.mcgill.ca/newsroom/files/newsroom/styles/media_gallery_large/public/mcgillcampus-9728_copy.jpg?itok=vVMl7vfS'};
 
 const App = () => (
-  <View style={styles.container}>
-   <ImageBackground source={image} resizeMode="cover" style={styles.image} blurRadius="2" >
-      <Text style={styles.title_text}>McGill Augmented Reality App</Text>
-      <View style={styles.spacing}/>
-      <Pressable style={styles.button} onPress={() => {console.log("Button pressed")}}>
-        <Text style={styles.btn_text}>Get Started</Text>
-      </Pressable>
-    </ImageBackground>
-  </View>
+  <HomeScreen/>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  spacing: {
-    justifyContent: 'center',
-    paddingVertical: 200,
-  },
-  title_text: {
-    color: 'red',
-    fontSize: 36,
-    lineHeight: 60,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
-  },
-  btn_text: {
-    fontSize: 20,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 18,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'red',
-  },
-});
 
 export default App;
---
+++ mcgill-augmented-reality-frontend/screens/HomeScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native';
+
+const image = {uri: 'https://wallpapercave.com/wp/wp10982822.jpg'};
+
+function HomeScreen(props) {
+    return (
+        <ImageBackground source={image} resizeMode="cover" style={styles.bgImage} blurRadius={0}>
+            <Image source={require('../assets/mcgill-icon.png')} style={styles.logo}/>
+            <Text style={styles.title}>AUGMENTED REALITY</Text>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.started}>Get Started</Text>
+            </TouchableOpacity>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        color: '#cf4037',
+        fontSize: 37,
+        fontWeight: 'bold',
+        letterSpacing: 2,
+        textAlign: 'center',
+        position: 'absolute',
+        top: 220
+    },
+    bgImage: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    button: {
+        width: '100%',
+        height: 80,
+        backgroundColor: "red",
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logo: {
+        backgroundColor: "#00000000",
+        flex: 1,
+        width: '70%',
+        height: '13%',
+        resizeMode: 'contain',
+        position: 'absolute',
+        top: 100,
+    },
+    started: {
+        color: 'white',
+        fontSize: 23,
+        fontFamily: 'AvenirNext-Medium',
+        letterSpacing: 2
+    }
+})
+
+export default HomeScreen;
