e56f8400f5c3a3e6fc6f15e47fbf44dcddc48937
Matthew Cabral <matthew.larbac@gmail.com>
2023-02-20 07:47:20 -0500

    Fixed Small Shortcode Issue
    The shortcode was not being.

--- mcgill-augmented-reality-backend/src/main/java/mcgillar/backend/model/building/Building.java
+++ mcgill-augmented-reality-backend/src/main/java/mcgillar/backend/model/building/Building.java
@@ -30,5 +30,6 @@ public class Building {
     public Building(Location location, String name, String shortCode) {
         this.location = location;
         this.name = name;
+        this.shortCode = shortCode;
     }
 }
