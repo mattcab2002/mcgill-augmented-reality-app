82ee7a4d4918174e40e3ad33f79c6c78616a6b5c
AriA700 <75389449+AriA700@users.noreply.github.com>
2023-02-21 18:14:20 -0500

    Add styling and remove button

--- mcgill-augmented-reality-frontend/components/FriendList.js
+++ mcgill-augmented-reality-frontend/components/FriendList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 
 const FriendList = () => {
   const [friends, setFriends] = useState([
@@ -9,20 +9,35 @@ const FriendList = () => {
     { id: '4', name: 'Mike' },
   ]);
 
+  const removeFriend = (id) => {
+    
+  };
+
   const renderFriend = ({ item }) => (
-    <View style={styles.friend}>
-      <Text style={styles.friendName}>{item.name}</Text>
+    <View style={styles.friendContainer}>
+      <View style={styles.avatarContainer}>
+        <Text style={styles.avatarText}>{item.name.charAt(0)}</Text>
+      </View>
+      <View style={styles.friendInfoContainer}>
+        <Text style={styles.friendName}>{item.name}</Text>
+        <Text style={styles.friendStatus}>Online</Text>
+      </View>
+      <TouchableOpacity onPress={() => removeFriend(item.id)}>
+        <Text style={styles.deleteButton}>X</Text>
+      </TouchableOpacity>
     </View>
   );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>My Friends List</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Friends List</Text>
+      </View>
       <FlatList
         data={friends}
         renderItem={renderFriend}
         keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.list}
+        contentContainerStyle={styles.listContainer}
       />
     </View>
   );
@@ -31,35 +46,64 @@ const FriendList = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
   },
-  title: {
+  titleContainer: {
+    height: 95,
     paddingTop: 50,
+    backgroundColor: '#CD202C',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    color: '#FFFFFF',
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'red',
-    marginBottom: 10,
   },
-  list: {
-    flexGrow: 1,
+  listContainer: {
+    paddingBottom: 60,
+  },
+  friendContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EDEDED',
+    alignItems: 'center',
+  },
+  avatarContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#FFAFAF',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  friend: {
-    backgroundColor: 'white',
-    marginVertical: 10,
-    padding: 10,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'red',
+  avatarText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  friendInfoContainer: {
+    flex: 1,
+    marginLeft: 20,
   },
   friendName: {
     fontSize: 18,
-    color: 'red',
     fontWeight: 'bold',
   },
+  friendStatus: {
+    fontSize: 14,
+    color: '#A0A0A0',
+  },
+  deleteButton: {
+    borderRadius: 50,
+    backgroundColor: '#FFAFAF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 10,
+    paddingHorizontal: 5
+  }
 });
 
 export default FriendList;
