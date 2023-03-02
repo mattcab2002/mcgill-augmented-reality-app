05dd2e542fa0046761c9dd7e2852d8a30aeb015b
Matthew Cabral <matthew.larbac@gmail.com>
2023-03-02 15:51:35 -0500

    Image Upload and Fetch Logic and formatting
    Most of the lines in this commit are due to formatting. The rest are related to the logic needed to fetch images from the db and also upload images to the db along with a schedule modal which can be used to view the current schedule if one is uploaded. The logic for deleting a user is still not in place and still need some logic for updating an image in the db.

--- mcgill-augmented-reality-frontend/components/AccountField.js
+++ mcgill-augmented-reality-frontend/components/AccountField.js
@@ -1,44 +1,49 @@
-import { View, Text, TextInput, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import fetchWrapper from '../api'
-import {BACKEND} from '@env';
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
 
 export default function AccountField(props) {
     const updateData = (ref) => {
         // use the ref of the input to get the value then make the api request to update
         // fetchWrapper(`${BACKEND}/route`)
-    }
+    };
 
-  return (
-    <View style={[styles.container, props.style]}>
-        <Text style={styles.textInputTitle}>{props.title ? props.title : "Input Field"}</Text>
-        <TextInput style={styles.textInput} editable={props.editable ? true : false} defaultValue={props.value} onEndEditing={updateData} />
-    </View>
-  )
+    return (
+        <View style={[styles.container, props.style]}>
+            <Text style={styles.textInputTitle}>
+                {props.title ? props.title : "Input Field"}
+            </Text>
+            <TextInput
+                style={styles.textInput}
+                editable={props.editable ? true : false}
+                defaultValue={props.value}
+                onEndEditing={updateData}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
         marginVertical: 4,
-        flex:1
+        flex: 1,
     },
     textInputTitle: {
-        color: '#ABABAB',
-        fontWeight: 'bold',
+        color: "#ABABAB",
+        fontWeight: "bold",
     },
     textInput: {
         borderRadius: 4,
-        backgroundColor: 'white',
-        borderColor: '#ABABAB',
+        backgroundColor: "white",
+        borderColor: "#ABABAB",
         borderWidth: 1,
-        fontWeight: 'bold',
-        color: 'black',
+        fontWeight: "bold",
+        color: "black",
         width: "100%",
         height: 45,
         paddingLeft: 10,
         marginTop: 6,
     },
-})
+});
--- mcgill-augmented-reality-frontend/components/ConfirmModal.js
+++ mcgill-augmented-reality-frontend/components/ConfirmModal.js
@@ -1,60 +1,104 @@
-import { Text, StyleSheet, Pressable, TextInput, Modal, View, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
-import fetchWrapper, {getToken} from '../api.js';
-import {BACKEND, USERNAME} from '@env';
+import {
+    Text,
+    StyleSheet,
+    Pressable,
+    TextInput,
+    Modal,
+    View,
+    TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import fetchWrapper, { getToken } from "../api.js";
+import { BACKEND, USERNAME } from "@env";
 
 export default function ConfirmModal(props) {
-  const [input, setInput] = useState('');
+    const [input, setInput] = useState("");
 
-  const confirmHandler = async () => {
-    // make request to token endpoint and check status to see if valid pass
-    const token = await getToken(USERNAME, input);   
-    if(token != null) {
-        // means valid password
-        fetchWrapper(`${BACKEND}/user/delete-account`, null, null, 'DELETE').then((res) => {
-//            if(res != something) {
-//                setErrorMessage and ask to try again
-//            } else
+    const confirmHandler = async () => {
+        // make request to token endpoint and check status to see if valid pass
+        const token = await getToken(USERNAME, input);
+        if (token != null) {
+            // means valid password
+            fetchWrapper(
+                `${BACKEND}/user/delete-account`,
+                null,
+                null,
+                "DELETE"
+            ).then((res) => {
+                //            if(res != something) {
+                //                setErrorMessage and ask to try again
+                //            } else
                 console.log("Account deleted.");
-        })
-    }
-    props.setVisibility(false);
-  }
-      
-  return (
+            });
+        }
+        props.setVisibility(false);
+    };
+
+    return (
         <View style={styles.centeredView}>
-            <Modal animationType="slide" transparent={true} visible={props.isVisible} onRequestClose={() => {props.setVisibility(false)}}>
-                <TouchableOpacity style={styles.centeredView} onPress={() => {props.setVisibility(false)}} activeOpacity={1}>
-                    <TouchableOpacity style={styles.modalView} activeOpacity={1}>
-                        <Text style={styles.prompt}>Please re-enter your {props.text} to confirm</Text>
-                        <TextInput style={styles.input} placeholder={props.placeholder} placeholderTextColor="#000" secureTextEntry={props.text == 'password' ? true : false} onChangeText={(text) => {setInput(text)}} />
-                        <Pressable onPress={confirmHandler} style={styles.confirmButton}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={props.isVisible}
+                onRequestClose={() => {
+                    props.setVisibility(false);
+                }}
+            >
+                <TouchableOpacity
+                    style={styles.centeredView}
+                    onPress={() => {
+                        props.setVisibility(false);
+                    }}
+                    activeOpacity={1}
+                >
+                    <TouchableOpacity
+                        style={styles.modalView}
+                        activeOpacity={1}
+                    >
+                        <Text style={styles.prompt}>
+                            Please re-enter your {props.text} to confirm
+                        </Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder={props.placeholder}
+                            placeholderTextColor="#000"
+                            secureTextEntry={
+                                props.text == "password" ? true : false
+                            }
+                            onChangeText={(text) => {
+                                setInput(text);
+                            }}
+                        />
+                        <Pressable
+                            onPress={confirmHandler}
+                            style={styles.confirmButton}
+                        >
                             <Text style={styles.confirmText}>Confirm</Text>
                         </Pressable>
                     </TouchableOpacity>
                 </TouchableOpacity>
             </Modal>
         </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
     centeredView: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         marginTop: 22,
     },
     modalView: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
         borderRadius: 20,
         padding: 35,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        shadowColor: '#000',
+        alignItems: "center",
+        justifyContent: "space-between",
+        shadowColor: "#000",
         shadowOffset: {
-          width: 0,
-          height: 2,
+            width: 0,
+            height: 2,
         },
         shadowOpacity: 0.25,
         shadowRadius: 4,
@@ -62,11 +106,11 @@ const styles = StyleSheet.create({
     },
     input: {
         borderRadius: 4,
-        backgroundColor: 'white',
-        borderColor: '#ED404C',
+        backgroundColor: "white",
+        borderColor: "#ED404C",
         borderWidth: 1,
-        fontWeight: 'bold',
-        color: 'black',
+        fontWeight: "bold",
+        color: "black",
         width: "100%",
         height: 45,
         paddingLeft: 10,
@@ -75,21 +119,21 @@ const styles = StyleSheet.create({
     },
     prompt: {
         marginBottom: 20,
-        fontSize: 16
+        fontSize: 16,
     },
     confirmButton: {
         marginTop: 6,
         height: 45,
-        backgroundColor: '#ED404C',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10, 
+        backgroundColor: "#ED404C",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
         borderRadius: 6,
     },
     confirmText: {
-        color: '#FFFFFF',
+        color: "#FFFFFF",
         fontSize: 16,
-        fontWeight: '500',
-    }
-})
+        fontWeight: "500",
+    },
+});
