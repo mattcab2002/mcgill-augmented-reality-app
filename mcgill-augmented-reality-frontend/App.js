70e3ec649a60bf5d8cbc8511d67ef80281add6e3
AriA700 <75389449+AriA700@users.noreply.github.com>
2023-03-11 17:06:03 -0500

    Removing some imports/comments

--- mcgill-augmented-reality-frontend/App.js
+++ mcgill-augmented-reality-frontend/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import FriendList from './components/FriendList';
 import NavigationBar from './components/NavigationBar';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      {/* <FriendList /> */}
       <NavigationContainer>
         <NavigationBar />
       </NavigationContainer>
