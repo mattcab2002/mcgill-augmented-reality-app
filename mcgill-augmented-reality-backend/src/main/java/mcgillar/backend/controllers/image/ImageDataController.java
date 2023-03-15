3843b3d22625bf991bd8f00f9ec7c3a4c36ec5f7
abhijeetpraveen <abhijeetpraveen@gmail.com>
2023-03-15 00:06:33 -0400

    Implementing OCR on schedule upload

--- mcgill-augmented-reality-backend/src/main/java/mcgillar/backend/controllers/image/ImageDataController.java
+++ mcgill-augmented-reality-backend/src/main/java/mcgillar/backend/controllers/image/ImageDataController.java
@@ -9,6 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.bytedeco.leptonica.global.lept;
+import org.bytedeco.leptonica.PIX;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.tesseract.*;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -22,9 +26,37 @@ public class ImageDataController {
 
     @PutMapping("schedule")
     public ResponseEntity<?> uploadScheduleImage(@RequestParam("image") MultipartFile file) throws IOException {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        try (TessBaseAPI api = new TessBaseAPI()){
+            // Convert MultipartFile to byte array
+            byte[] fileBytes = file.getBytes();
 
-        imageDataService.uploadImage(file, "schedule_" + name);
+            // Create PIX object from byte array
+            PIX image = lept.pixReadMem(fileBytes, fileBytes.length);
+
+            // OCR using Tesseract
+            BytePointer outText;
+
+            if (api.Init(null, "eng") != 0) {
+                System.err.println("Could not initialize tesseract.");
+                System.exit(1);
+            }
+
+            api.SetImage(image);
+            outText = api.GetUTF8Text();
+
+            // Print OCR result to console
+            System.out.println("OCR output:\n" + outText.getString());
+
+            api.End();
+            outText.deallocate();
+            lept.pixDestroy(image);
+
+            String name = SecurityContextHolder.getContext().getAuthentication().getName();
+            imageDataService.uploadImage(file, "schedule_" + name);
+        } catch(IOException error) {
+            System.err.println(error.getMessage());
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
 
         return new ResponseEntity(HttpStatus.OK);
     }
