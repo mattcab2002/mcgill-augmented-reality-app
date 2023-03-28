c227d3b30c4f037f9080c1510509d1fac0e9eb5f
Matthew Cabral <matthew.larbac@gmail.com>
2023-03-28 17:41:58 -0400

    Reverting webEnvironment change

--- mcgill-augmented-reality-backend/src/test/java/mcgillar/backend/user/LoginTest.java
+++ mcgill-augmented-reality-backend/src/test/java/mcgillar/backend/user/LoginTest.java
@@ -7,6 +7,7 @@ import java.util.Base64;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,7 +16,7 @@ import org.springframework.http.ResponseEntity;
 
 import mcgillar.backend.services.user.AppUserService;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class LoginTest {
     
     @Autowired
