ac15ac3dcb1c34f664032969cda5231cc78b9f19
Matthew Cabral <matthew.larbac@gmail.com>
2023-03-28 17:10:45 -0400

    Seeing if this fixes things

--- mcgill-augmented-reality-backend/src/test/java/mcgillar/backend/user/LoginTest.java
+++ mcgill-augmented-reality-backend/src/test/java/mcgillar/backend/user/LoginTest.java
@@ -7,7 +7,6 @@ import java.util.Base64;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 
 import mcgillar.backend.services.user.AppUserService;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class LoginTest {
     
     @Autowired
